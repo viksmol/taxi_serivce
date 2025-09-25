921368dc9c25acebc92f7c4baedc359921875272
viksmol <leoniddemcuk403@gmail.com>
2025-09-25 17:37:20 +0300

    створення адмін-сторіеку

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from taxi.models import Driver, Manufacturer, Car
+
+def index(requst):
+    num_drivers =Driver.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+    num_car = Car.objectc.count()
+
+    cotext = {
+        "num_drivers": num_drivers,
+        "num_manufacturers": num_manufacturers,
+        "num_car": num_car,
+        }
+    return render(
+        requst, "taxi/index.html", context=cotext
+        )
